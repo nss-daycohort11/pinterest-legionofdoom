1e002f13dc96384c156913b1d112f89937a34da9
Mathew Ostrander <mathew.ostrander@gmail.com>
2015-12-08 14:21:30 -0600

    all things from all users display to the main board, creating personal board is next

--- app/controllers/Board.js
+++ app/controllers/Board.js
@@ -2,8 +2,8 @@ app.controller("Board",
 	["$q", "$scope", "$rootScope", "$firebaseArray", "Auth", "$location",
 	function($Q, $scope, $rootScope, $firebaseArray, Auth, $location) {
 
-    var fireBoard = new Firebase("https://legionofdoom.firebaseio.com/users/" + Auth.getUid() + "/");
-    // var fireBoard = new Firebase("https://legionofdoom.firebaseio.com/users/");
+    // var fireBoard = new Firebase("https://legionofdoom.firebaseio.com/users/" + Auth.getUid() + "/");
+    var fireBoard = new Firebase("https://legionofdoom.firebaseio.com/users/");
     console.log("fireBoard", fireBoard);
     $scope.pins = $firebaseArray(fireBoard);
    
