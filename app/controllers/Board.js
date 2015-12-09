688c68a53e2b9dd1999c6fa99805a209366188cd
Michael Conrad <michael@threepears.com>
2015-12-09 10:21:24 -0600

    Create duplicate filter

--- app/controllers/Board.js
+++ app/controllers/Board.js
@@ -4,20 +4,61 @@ app.controller("Board",
 
     // var fireBoard = new Firebase("https://legionofdoom.firebaseio.com/users/" + Auth.getUid() + "/");
     var fireBoard = new Firebase("https://legionofdoom.firebaseio.com/users/");
-    console.log("fireBoard", fireBoard);
-    $scope.pins = $firebaseArray(fireBoard);
+    // console.log("fireBoard", fireBoard);
+/*    $scope.pins = $firebaseArray(fireBoard);
+*/
+    var entries = $firebaseArray(fireBoard);
+    console.log(entries);
+
+    var removeDupes = [];
+    var newArray = {};
+
+    entries.$loaded()
+    .then(function(){
+        angular.forEach(entries, function(entry) {
+            angular.forEach(entry, function(widget) {
+                    var added = false;
+            		if (widget === null || widget.title === null || !widget.title) {
+            			console.log("Nope!");
+            			console.log(widget);
+            		} else if (widget.title) {
+                        console.log("Inside title");
+                        angular.forEach(removeDupes, function(item) {
+                            console.log("Inside removeDupes");
+                            console.log("Item title", item.title);
+                            console.log("Widget title", widget.title);
+                            if (item.title === widget.title) {
+                                added = true;
+                            }
+                        });
+            			if (added === false) {
+                            removeDupes.push(widget);
+                        }
+            		}
+            });
+        });
+        console.log("newArrayAfter", removeDupes);
+        for (var i = 0; i < removeDupes.length; i++) {
+        	newArray[i] = removeDupes[i];
+        }
+        $scope.pins = [newArray];
+    });
    
-      console.log("fireBoardArray", $scope.pins);
+
+
     
-	  $scope.addToBoard = function (notStuff) {
+
+
+    
+	$scope.addToBoard = function (notStuff) {
 	    console.log("stuff", this.stuff);
 	    var BoardRef = new Firebase("https://legionofdoom.firebaseio.com/users/" + Auth.getUid() + "/");
-	    BoardRef = $firebaseArray(BoardRef)
+	    BoardRef = $firebaseArray(BoardRef);
 
 	      
 	    console.log("BoardRef", BoardRef);
 	    BoardRef.$add(notStuff);
-		};
+	};
 
 
 }]);
